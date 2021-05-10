699412a16af2da4ad5c2c9bdab14ad1c62bea856
pichuzhkinaen <88-katerina@mail.ru>
2021-05-10 22:02:52 +0300

    regexp calc area, create span calc-prev, addition input num and calc-prev num

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,93 @@
 window.addEventListener('DOMContentLoaded', function() {
     'use strict';
 
-    const calcArea = document.querySelector('.calc-area'),
+    const calcAreaWrapper = document.querySelector('.calc-area'),
+        calcArea = document.querySelector('.calc-area-input'),
         numbers = document.querySelectorAll('.numbers-button'),
-        plusMinus = document.getElementById('#plus-minus'),
-        plus = document.getElementById('#plus'),
-        minus = document.getElementById('#minus');
+        plusMinus = document.getElementById('plus-minus'),
+        plus = document.getElementById('plus'),
+        minus = document.getElementById('minus'),
+        comma = document.getElementById('comma');
+
+    let dataInput = [];
+
+    // console.log(typeof calcArea.value); //string
 
     calcArea.addEventListener('focus', () => {
         if (calcArea.value == 0) {
             calcArea.value = '';
         }
     });
+    
+    calcArea.addEventListener('input', (e) => {
+        // const newInputValue = calcArea.value.match(/(\d+[.,]\d+)|(\d+)/g);
+        // console.log(/(\d+[.,]\d+)|(\d+)/g.test(newInputValue));
+        // console.log(newInputValue);
+        
 
-    calcArea.addEventListener('input', () => {
-        const newInputValue = calcArea.value.replace(/[^\d\.]/g, '').replace( /^([^\.]*\.)|\./g, '$1' );
+        // if (calcAreaPrev.innerHTML.includes('+')) {
+            // calcArea.value = '';
+            // let eData = [e.data];
+            // eData.push(e.data);
+            // console.log(eData);
+            // calcArea.value += '' + eData;
+        // }
+
+        const newInputValue = calcArea.value.replace(/[^\d\.]/g, '').replace( /^([^\.]*\.)|\./g, '$1' );       
         calcArea.value = newInputValue;
-        console.log(newInputValue);
+    });
+
+    plus.addEventListener('click', () => {
+        createCalcPrev();
+        let calcPrev = document.querySelector('.calc-area-prev');
+        
+        if (!calcPrev.innerHTML == '') {
+            calcPrev.innerHTML = calcPrev.innerHTML + calcArea.value + '+';
+            dataInput.push(calcArea.value);
+            let dataInputSum = 0;
+
+            for (let i = 0; i < dataInput.length; i++) {
+                dataInput[i] = Number(dataInput[i]);
+                dataInputSum += dataInput[i];
+                calcPrev.innerHTML = dataInputSum + ' +';
+                calcArea.value = '';
+            }
+        } else {
+            calcPrev.innerHTML = calcArea.value + ' +';
+            dataInput.push(calcArea.value);
+            calcArea.value = '';
+        }
+        // calcArea.value = calcArea.value + dataInput;
+        // calcPrev.innerHTML = calcArea.value;
+    });
+
+    // comma.addEventListener('click', () => {
+    //     if (!calcArea.value.includes(',')) {
+    //         calcArea.value += ',';
+    //         console.log(comma);
+
+    //     }
+    // });
+
+    plusMinus.addEventListener('click', () => {
+        if (calcArea.value != -calcArea.value) {
+            calcArea.value = -calcArea.value;
+            console.log(typeof calcArea.value); //string
+        } else {
+            calcArea.value = calcArea.value;
+        }
+    });
+
+    dot.addEventListener('click', () => {
+        if (!calcArea.value.includes('.')) {
+            calcArea.value += dot.innerHTML;
+        }
+    });
+    
+
+    //установка фокуса на input при клике на кнопки клавиатуры
+    document.addEventListener('keydown', (e) => {
+        calcArea.focus();
     });
 
     //заполнение поля инпут при клике на кнопки с цифрами
@@ -40,4 +111,9 @@ window.addEventListener('DOMContentLoaded', function() {
         i++;
     }
 
+    function createCalcPrev() {
+        let calcPrev = document.createElement('span');
+        calcAreaWrapper.append(calcPrev);
+        calcPrev.classList.add('calc-area-prev');
+    }
 });
