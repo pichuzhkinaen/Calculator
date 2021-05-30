5d6c97b7fe8daad844bd5fac8e267250662df87a
pichuzhkina <88-katerina@mail.ru>
2021-05-30 22:50:45 +0300

    calculate operation, equals

--- src/js/script.js
+++ src/js/script.js
@@ -8,23 +8,50 @@ window.addEventListener('DOMContentLoaded', function() {
         plusMinus = document.getElementById('plus-minus'),
         plus = document.getElementById('plus'),
         minus = document.getElementById('minus'),
-        comma = document.getElementById('comma');
+        comma = document.getElementById('comma'),
+        equals = document.getElementById('equals'),
+        operator = document.querySelectorAll('.button-operator');
 
-    let result = 0;
-    let inputValue = '';
-        
+    let result = 0,
+        inputValue = '',
+        counter = 1;
+
+    calcArea.addEventListener('focus', () => {
+        if (calcArea.value == 0) {
+            calcArea.value = '';
+        }
+    });
+
+    calcArea.addEventListener('input', () => {
+        validateInputValue();
+    });
+
     const addNumInput = (e) => {
         const target = e.target;
         const btnValue = target.value;
 
-        if (inputValue == '') {
+        if (counter === 0) {
+            calcPrev.innerHTML = '';
             calcArea.value = btnValue;
-            inputValue = calcArea.value;
         } else {
-            calcArea.value += btnValue;
-            inputValue = calcArea.value;
+            if (inputValue == '') {
+                calcArea.value = btnValue;
+                inputValue = calcArea.value;
+            } else {
+                calcArea.value += btnValue;
+                inputValue = calcArea.value;
+            }
         }
     }
+
+    // const newInputValue = calcArea.value.replace(/[^\d\.]/g, '').replace( /^([^\.]*\.)|\./g, '$1' );       
+    //     calcArea.value = newInputValue;
+
+    // const validateInputValue = () => {
+    //     inputValue = calcArea.value.match(/\d+\.?/g);
+    //     calcArea.value = inputValue;
+    //     console.log(inputValue);
+    // }
         
     let i = 0;
     while (i < numbersBtn.length) {
@@ -32,29 +59,77 @@ window.addEventListener('DOMContentLoaded', function() {
         i++;
     }
 
-    const addDataCalcPrev = () => {
+    const addDataCalcPrev = (e) => {
+        const target = e.target;
+
         inputValue = calcArea.value;
 
         calcArea.value = '';
         
-        let PrevInner = calcPrev.innerHTML;
+        let prevInner = calcPrev.innerHTML;
 
-        if (PrevInner === '') {
-            calcPrev.innerHTML = inputValue + ' + ';
+        if (prevInner === '') {
+            calcPrev.innerHTML = inputValue + ' ' + target.innerHTML + ' ';
         } else {
-            calcPrev.innerHTML = PrevInner + inputValue + ' + ';
+            calcPrev.innerHTML = prevInner + inputValue + ' ' + target.innerHTML + ' ';
         }
-
-        addition(inputValue);
+        calculate(target);
     }
 
-    const addition = () => {
-        result += +inputValue;
-        calcArea.value = result;
-        inputValue = '';
-        console.log(inputValue);
+    const calculate = (target) => {
+        
+        switch (target.innerHTML) {
+            case '+':
+                result += +inputValue;
+                calcArea.value = result;
+                inputValue = '';
+                break;
+            case '-':
+                result -= +inputValue;
+                calcArea.value = result;
+                inputValue = '';
+                break;
+            case '*':
+                result *= +inputValue;
+                calcArea.value = result;
+                inputValue = '';
+                break;
+            case '/':
+                result /= +inputValue;
+                calcArea.value = result;
+                inputValue = '';
+                break;
+            case '%':
+                result = result / 100 * +inputValue;
+                calcArea.value = result;
+                inputValue = '';
+                break;
+            default:
+                alert( "target" );
+        }  
     }
     
-    plus.addEventListener('click', addDataCalcPrev);
+    for (i = 0; i < operator.length; i++) {
+        operator[i].addEventListener('click', addDataCalcPrev);
+    }
+
+    plusMinus.addEventListener('click', () => {
+        if (calcArea.value != -calcArea.value) {
+            calcArea.value = -calcArea.value;
+            // console.log(typeof calcArea.value); //string
+        }
+    });
+
+    equals.addEventListener('click', () => {
+
+        if (counter === 0) {
+            calcPrev.innerHTML = calcPrev.innerHTML;
+            calcArea.value = calcArea.value;
+        } else {
+            calcPrev.innerHTML = calcPrev.innerHTML + ' ' + calcArea.value;
+            calcArea.value = result + +calcArea.value;
+            counter = 0;
+        }
+    });
 
 });
